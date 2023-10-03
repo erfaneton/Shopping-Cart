ad76aa47bacfc50806915676f28e66fb26b2722f
erfaneton <erfanshafiei.ea@gmail.com>
2023-10-03 22:34:20 +0330

    Feat: Store page & data file

---
+++ src/components/StoreItem/StoreItem.tsx
@@ -0,0 +1,22 @@
+import { Card } from 'react-bootstrap'
+
+type StoreItemProps = {
+    id: number
+    name: string
+    price: number
+    img: string
+}
+
+
+export default function StoreItem({ id, name, price, img }: StoreItemProps) {
+    return (
+        <Card>
+            <Card.Img variant='top' src={img} height='400px' style={{ objectFit: 'cover' }} />
+            <Card.Body className='d-flex flex-coulumn' ></Card.Body >
+            <Card.Title className='d-flex justify-content-between align-item-baseline mb-4'>
+                <span className='fs-2'>{name}</span>
+                <span className='ms-2 text-muted'>{price}</span>
+            </Card.Title>
+        </Card>
+    )
+}
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,17 @@
+import { Col, Row } from 'react-bootstrap'
+import storeItems from '../data/Items.json'
+import StoreItem from '../components/StoreItem/StoreItem'
+
+
 export default function Store() {
     return (
-        <div>Store</div>
+        <>
+            <div>store</div>
+            <Row lg={3} md={2} xs={1}>
+                {storeItems.map(item => (
+                    <Col key={item.id}><StoreItem {...item} /></Col>
+                ))}
+            </Row>
+        </>
     )
 }
