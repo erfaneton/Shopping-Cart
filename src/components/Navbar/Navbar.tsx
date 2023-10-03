f605c5bbe4363aa6800731f8e4d54b1eecb4c773
erfaneton <erfanshafiei.ea@gmail.com>
2023-10-03 18:58:58 +0330

    Feat: Navbar with bootstarp

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { Container, Nav, Navbar as NavbarBs, Button } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+import { FiShoppingCart } from 'react-icons/fi'
+import './Navbar.css'
 
 export default function Navbar() {
     return (
-        <div>Navbar</div>
+        <NavbarBs sticky='top' className='bg-white shadow-sm mb-3'>
+            <Container>
+                <Nav>
+                    <Nav.Link to='/' as={NavLink}>Home</Nav.Link>
+                    <Nav.Link to='/store' as={NavLink}>Store</Nav.Link>
+                    <Nav.Link to='/about' as={NavLink}>About</Nav.Link>
+                </Nav>
+                <Button style={{ width: '3rem', height: '3rem', position: 'relative' }}
+                    variant='outline-primary'
+                    className='rounded-circle'><FiShoppingCart className='icon' />
+                    <div className='rounded-circle bg-danger d-flex justify-content-center align-item-center' style={{ color: 'white', width: '1.5rem', height: '1.5rem', position: 'absolute', bottom: 0, right: 0, transform: 'translate(23%,23%)' }}>3</div>
+                </Button>
+            </Container>
+        </NavbarBs>
+
     )
 }
