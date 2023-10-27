24969a9781caae77c238dafccfbd153affc447bb
erfaneton <erfanshafiei.ea@gmail.com>
2023-10-27 17:30:26 +0330

    Refactore: imgages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <>
-            <img src="public/imgs/library.avif" alt="" style={{ width: '96rem', height: '100%', position: 'absolute', left: '0rem', bottom: '-2rem', objectFit: 'cover' }} />
+            <img src="/imgs/library.avif" alt="" style={{ width: '96rem', height: '100%', position: 'absolute', left: '0rem', bottom: '-2rem', objectFit: 'cover' }} />
             <h1 style={{ position: 'absolute', zIndex: '1', color: 'white', left: '45%', bottom: '50%' }}>WELCOME :)</h1>
         </>
     )
